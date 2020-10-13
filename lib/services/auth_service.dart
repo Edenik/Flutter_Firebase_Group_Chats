9e7ef146f59d22dacdcbbe13a272ac0257018a15
Edenik <Edenik5@gmail.com>
2020-10-13 18:15:23 +0300

    Authentication Service

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,45 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_chat/utilities/constants.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/services.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseMessaging _messaging = FirebaseMessaging();
+
+  Stream<FirebaseUser> get user => _auth.onAuthStateChanged;
+
+  Future<void> signup(String name, String email, String password) async {
+    try {
+      AuthResult authResult = await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      if (authResult.user != null) {
+        String token = await _messaging.getToken();
+        userRef.document(authResult.user.uid).setData({
+          'name': name,
+          'email': email,
+          'token': token,
+        });
+      }
+    } on PlatformException catch (err) {
+      throw (err);
+    }
+  }
+
+  Future<void> login(String email, String password) async {
+    try {
+      await _auth.signInWithEmailAndPassword(email: email, password: password);
+    } on PlatformException catch (err) {
+      throw (err);
+    }
+  }
+
+  Future<void> logout() {
+    Future.wait([
+      _auth.signOut(),
+    ]);
+  }
+}
---
+++ lib/utilities/constants.dart
@@ -0,0 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+final Firestore _db = Firestore.instance;
+final userRef = _db.collection('users');
+final chatsRef = _db.collection('chats');
