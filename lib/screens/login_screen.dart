e2f9ed9825e05eb7ed636e43d8e151b11226ad03
Edenik <Edenik5@gmail.com>
2020-10-13 19:13:37 +0300

    Login Signup Screen - Part 1

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -6,8 +6,87 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  int _selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Welcome!',
+              style: TextStyle(fontSize: 30.0, fontWeight: FontWeight.w600),
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Container(
+                  width: 150.0,
+                  child: FlatButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    color: _selectedIndex == 0 ? Colors.blue : Colors.grey[300],
+                    child: Text(
+                      'Login',
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          color:
+                              _selectedIndex == 0 ? Colors.white : Colors.blue),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _selectedIndex = 0;
+                      });
+                    },
+                  ),
+                ),
+                Container(
+                  width: 150.0,
+                  child: FlatButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    color: _selectedIndex == 1 ? Colors.blue : Colors.grey[300],
+                    child: Text(
+                      'Sign Up',
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          color:
+                              _selectedIndex == 1 ? Colors.white : Colors.blue),
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _selectedIndex = 1;
+                      });
+                    },
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(height: 20.0),
+            Container(
+              width: 180.0,
+              child: FlatButton(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                color: Colors.blue,
+                child: Text(
+                  'Submit',
+                  style: TextStyle(color: Colors.white, fontSize: 20.0),
+                ),
+                onPressed: () {},
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
